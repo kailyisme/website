2dcc881460609bb5faa100af9c39376372f8fe08
kaily <kaily.a.newman@gmail.com>
2021-06-22 00:09:58 +0100

    changed to http listener due to iisnode

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const serverApp = require("./src/serverApp");
 const { appendToLog } = require("./src/utils");
+const http = require("http");
 
 const PORT = 9091;
 if (process.env.PORT) PORT = process.env.PORT;
 
-serverApp.listen(PORT, () => {
+http.createServer(serverApp).listen(PORT, () => {
   appendToLog(`Listening to port: ${PORT}`);
 });
