b8fb989cc8a34b17bb96e070e6d71f91a1bc0ea3
kaily <kaily.a.newman@gmail.com>
2021-06-21 23:34:09 +0100

    changed port in case process.env.PORT exists for Production Environment in Plesk

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const serverApp = require("./src/serverApp");
 const { appendToLog } = require("./src/utils");
 
 const PORT = 9091;
+if (process.env.PORT) PORT = process.env.PORT;
 
 serverApp.listen(PORT, () => {
   appendToLog(`Listening to port: ${PORT}`);
